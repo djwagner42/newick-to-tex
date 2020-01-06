e43f973882ceb788e6ef1130192ecd0f5fa8f681
william bordowitz <wnb8537@truman.edu>
2020-01-06 15:43:46 -0600

    update generate tree

--- generateTree.py
+++ generateTree.py
@@ -1,2 +1,12 @@
+### We start with some kind of regex shit. I need to learn how to do 
+### regex shit. Whoops
+import re
+import sys
+
 def main():
+    texDoc = open('treeDoc.txt', 'w') #Maybe make this a legit tex file
+    texString = readtree(argv[1])
+    texDoc.write(texString)
+
+def readtree(tree):    
     
